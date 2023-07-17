879ec36d197b24f99f2a8d75e867d50c1aa41221
natejmaster@gmail.com <natejmaster@gmail.com>
2023-07-17 15:01:58 -0500

    Copied acceptance criteria to readme temporarily for convenience, added declaration of Save Button variable in JS and wrote framework for onClick event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+let saveButton = $('#saveBtn')
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -8,7 +9,9 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+saveButton.on('click'). function () {
+  
+}
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
